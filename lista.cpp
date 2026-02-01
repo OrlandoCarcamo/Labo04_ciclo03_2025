4d6b8d7dff763317d79fa5671e3ad330405651da
OrlandoCarcamo <00370825@uca.edu.sv>
2026-01-31 18:55:23 -0600

    add; se agrego la funcion para eliminar un elemento por medio del id

--- lista.cpp
+++ lista.cpp
@@ -90,6 +90,46 @@ void mostrarAtras(Nodo* tail) {
     }
 }
 
+bool eliminar(Nodo*& head, Nodo*& tail, int id ){
+    if (head == nullptr) {
+        cout << "La lista esta vacia"<< endl;
+        return false;
+    }
+    Nodo* borrar = head;
+    
+    while (borrar != nullptr){
+
+        if (borrar->id == id){
+            if (head == tail){
+                head = tail = nullptr;
+            }
+            else if (borrar == head){
+                head = head->sig;
+                head->ant= nullptr;
+            }
+
+            else if (borrar == tail){ 
+            tail = tail->ant;
+            tail->sig= nullptr; 
+            }
+            else {
+            borrar->ant->sig = borrar->sig;
+            borrar->sig->ant= borrar->ant;
+            }
+            delete borrar;
+            cout << "Elemento eliminado. " << endl;
+            return true;
+    }
+   
+    borrar = borrar->sig;
+} 
+
+        cout << "No se ha encontrado el id. " << endl;
+        return false;
+    
+    }
+
+
 
 int main(){
 
